b2a825c249bf90bf518e608f3f66bfa898005709
Administrator <soongon@a.a>
2019-04-04 11:58:49 +0900

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MovieModel} from '../model/movie.model';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -15,12 +15,31 @@ export class HomePage implements OnInit{
 
   // 네이버 접속해서 데이터를 가져온다.
   ngOnInit(): void {
-    this.http.get<MovieModel[]>('assets/movies.json')
+    // 파라미터 설정
+    const params = new HttpParams()
+        .set('query','극한직업');
+    // 헤더 설정
+    const headers = new HttpHeaders()
+        .set('X-Naver-Client-Id','vYGdytfBsHznI2IuhhGt')
+        .set('X-Naver-Client-Secret','uyJjxWtwU8');
+
+    this.http.get<MovieModel[]>(
+        'https://openapi.naver.com/v1/search/movie.json',
+        {params, headers})
         .subscribe(res => {
           this.movies = res;
           console.log(this.movies);
         });
     console.log('code.........................');
+
+    //this.getGithub();
+  }
+
+  getGithub() {
+    this.http.get('https://api.github.com/users/soongon')
+        .subscribe(res => {
+          console.log(res);
+        });
   }
 
 }
