bb10c45647bdb59207fb2166fe936e1fa8392b91
Administrator <soongon@a.a>
2019-04-04 10:48:43 +0900

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MovieModel} from '../model/movie.model';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  movies: MovieModel[];
+
+  constructor(private http: HttpClient) {}
+
+  // 네이버 접속해서 데이터를 가져온다.
+  ngOnInit(): void {
+
+  }
 
 }
---
+++ src/app/model/movie.model.ts
@@ -0,0 +1,10 @@
+export class MovieModel {
+    title: string;
+    link: string;
+    image: string;
+    subtitle: string;
+    pubDate: string;
+    director: string;
+    actor: string;
+    userRating: string;
+}
